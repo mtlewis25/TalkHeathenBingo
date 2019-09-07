d34f1e8e91366ebf1217c656f55a0134c88026e2
Dan Burgess <frenchrieman2@@gmail.com>
2019-09-07 17:29:23 -0400

    BINGO-1: added the ability to toggle selected on bingo card

---
+++ Resources/Bingo.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+}
+
+function select(element) {
+  if(element.classList.contains("selected")){
+    element.classList.remove("selected");
+  } else {
+    element.classList.add("selected");
+  }
+}
